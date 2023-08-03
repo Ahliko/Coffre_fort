95146f109f0cf546c4a7a8f496e437c42a78ae8d
Ahliko <killian.guillemot@ynov.com>
2023-08-03 16:32:38 +0200

    Add file

--- Coffre_1/test.py
+++ Coffre_1/test.py
@@ -1,8 +1,10 @@
 from machine import UART
 import time
 
-uart = UART(2, baudrate=115200, tx=17, rx=16)
-uart.write("hello")
-for i in range(1000):
+uart = UART(0, baudrate=115200, tx=17, rx=16)
+
+for i in range(10):
+    uart.write("hello")
+    time.sleep(0.1)
     print(uart.read(16))
     time.sleep(0.1)
