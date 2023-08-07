d8ac28074d6c74f3642c22d7bf8d6d76a5736b61
Ahliko <killian.guillemot@ynov.com>
2023-08-07 17:03:33 +0200

    Update file

--- Coffre_2/server/index.py
+++ Coffre_2/server/index.py
@@ -1,30 +1,37 @@
-import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import sqlite3
 
 app = Flask(__name__)
 
+connect = False
 
-class Data:
-    def __init__(self, name):
-        self.name = name
-        self.age = 20
+
+def check_user(username, password):
+    conn = sqlite3.connect('db.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM Users')
+    users = c.fetchall()
+    for user in users:
+        if user[1] == username and user[2] == password:
+            return True
+    return False
 
 
 @app.route('/')
 def index():
-    data = Data("John")
-    return render_template(flask.url_for('index.html'), data=data)
+    return render_template('index.html', data=connect)
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/song')
-def song():
-    return app.redirect('/about')
+@app.route('/verify', methods=['POST'])
+def verify():
+    if check_user(request.form['username'], request.form['password']):
+        connect = True
+        print('User connected')
+    else:
+        connect = False
+        print('User not connected')
+    return redirect('/')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
