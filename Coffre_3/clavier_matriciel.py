9d8cae23fbc9341e74a26d6fe286e8f4a237d25a
Ahliko <killian.guillemot@ynov.com>
2023-08-11 11:16:53 +0200

    Update file

--- Coffre_3/clavier_matriciel.py
+++ Coffre_3/clavier_matriciel.py
@@ -1,55 +1,49 @@
 import RPi.GPIO as GPIO
 import time
 
-# Configuration des broches du clavier matriciel
-ROWS = [5, 6, 13, 19]  # Broches connectées aux lignes
-COLS = [12, 16, 20, 21]  # Broches connectées aux colonnes
 
-# Matrice représentant les touches du clavier
-keys = [
-    ['1', '2', '3', 'F'],
-    ['4', '5', '6', 'E'],
-    ['7', '8', '9', 'D'],
-    ['A', '0', 'B', 'C']
-]
+class Clavier:
+    def __init__(self):
+        self.ROWS = [5, 6, 13, 19]  # Broches connectées aux lignes
+        self.COLS = [12, 16, 20, 21]  # Broches connectées aux colonnes
+        self.keys = [
+            ['1', '2', '3', 'F'],
+            ['4', '5', '6', 'E'],
+            ['7', '8', '9', 'D'],
+            ['A', '0', 'B', 'C']
+        ]
 
+    def setup_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        for row in self.ROWS:
+            GPIO.setup(row, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        for col in self.COLS:
+            GPIO.setup(col, GPIO.OUT)
+            GPIO.output(col, 1)
 
-# Configuration initiale des broches GPIO
-def setup_gpio():
-    GPIO.setmode(GPIO.BCM)
-    for row in ROWS:
-        GPIO.setup(row, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    for col in COLS:
-        GPIO.setup(col, GPIO.OUT)
-        GPIO.output(col, 1)
+    def detect_key(self):
+        key = None
+        for col_num, col_pin in enumerate(self.COLS):
+            GPIO.output(col_pin, 0)
+            for row_num, row_pin in enumerate(self.ROWS):
+                if not GPIO.input(row_pin):
+                    key = self.keys[row_num][col_num]
+                    while not GPIO.input(row_pin):
+                        pass
+            GPIO.output(col_pin, 1)
+        return key
 
+    @staticmethod
+    def cleanup():
+        GPIO.cleanup()
 
-# Fonction pour détecter la touche pressée
-def detect_key():
-    key = None
-    for col_num, col_pin in enumerate(COLS):
-        GPIO.output(col_pin, 0)
-        for row_num, row_pin in enumerate(ROWS):
-            if not GPIO.input(row_pin):
-                key = keys[row_num][col_num]
-                while not GPIO.input(row_pin):
-                    pass
-        GPIO.output(col_pin, 1)
-    return key
-
-
-# Nettoyage des broches GPIO
-def cleanup():
-    GPIO.cleanup()
-
-
-if __name__ == "__main__":
-    try:
-        setup_gpio()
-        while True:
-            pressed_key = detect_key()
-            if pressed_key:
-                print("Touche pressée:", pressed_key)
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        cleanup()
+    def run(self):
+        try:
+            self.setup_gpio()
+            while True:
+                pressed_key = self.detect_key()
+                if pressed_key:
+                    print("Touche pressée:", pressed_key)
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            self.cleanup()
